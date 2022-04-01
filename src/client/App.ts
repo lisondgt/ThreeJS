a929f396d417d9d1e72c1f9fc40a0de0c951e3be
lison.dagostini <lison.dagostini@gmail.com>
2022-04-01 18:06:55 +0200

    commit

--- src/client/App.ts
+++ src/client/App.ts
@@ -1,11 +1,23 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
 
 export class App {
+  rotationX = 0.01;
+  rotationY = 0.01;
+  cubeWidth = 1;
+  cubeHeight = 1;
+  cubeDepth = 1;
+
   private constructor(
     private readonly _renderer: THREE.Renderer,
     private readonly _scene: THREE.Scene,
     private readonly _camera: THREE.PerspectiveCamera,
-    private readonly _cube: THREE.Mesh
+    private readonly _cube: THREE.Mesh,
+    private readonly _orbitControls: OrbitControls,
+    private readonly _stats: Stats,
+    private readonly _gui: GUI
   ) {
     this._scene.add(this._cube);
   }
@@ -24,9 +36,13 @@ export class App {
 
   private _proccessFrame(t: number) {
     requestAnimationFrame(this._proccessFrame.bind(this));
-    this._cube.rotation.x += 0.01;
-    this._cube.rotation.z += 0.01;
+    this._cube.rotation.x += this.rotationX;
+    this._cube.rotation.z += this.rotationY;
+    this._cube.scale.x = this.cubeWidth;
+    this._cube.scale.y = this.cubeHeight;
+    this._cube.scale.z = this.cubeDepth;
     this._render();
+    this._stats.update();
   }
 
   run() {
@@ -47,8 +63,27 @@ export class App {
         wireframe: true,
       })
     );
+    const orbitControls = new OrbitControls(camera, renderer.domElement);
+    const stats = Stats();
     document.body.appendChild(renderer.domElement);
-    const res = new App(renderer, scene, camera, cube);
+    document.body.appendChild(stats.dom);
+    const gui = new GUI();
+    const res = new App(
+      renderer,
+      scene,
+      camera,
+      cube,
+      orbitControls,
+      stats,
+      gui
+    );
+    const rotationFolder = gui.addFolder("Cube rotation");
+    rotationFolder.add(res, "rotationX", 0, 1, 0.01);
+    rotationFolder.add(res, "rotationY", 0, 1, 0.01);
+    const sizeFolder = gui.addFolder("Cube size");
+    sizeFolder.add(res, "cubeWidth", 0.1, 10, 0.1);
+    sizeFolder.add(res, "cubeHeight", 0.1, 10, 0.1);
+    sizeFolder.add(res, "cubeDepth", 0.1, 10, 0.1);
     window.addEventListener("resize", res._onWindowResize.bind(res), false);
     return res;
   }
